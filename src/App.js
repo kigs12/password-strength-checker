e4a04d0bd2b3bddb86ce10c6daf98777d907ad77
kigs12 <mortalworld72@gmail.com>
2025-07-02 21:14:17 +0800

    Updated the Button Name

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PasswordStrengthChecker from "./components/PasswordStrengthChecker";
+
+
+function App() {
+  return (
+  <PasswordStrengthChecker />
+);
+
+}
+
+export default App;
